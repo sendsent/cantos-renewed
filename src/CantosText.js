d31ff90fa96fff58ca3297f8912daba61d660908
FJF616 <franzjfernando@hotmail.com>
2022-05-03 02:07:56 -0700

    corrections to canto text, turns spell check off

--- src/CantosText.js
+++ src/CantosText.js
@@ -170,7 +170,7 @@ const preventPasteNegative = (e) => {
          indexes.map((cantoIndex, index) => 
          <div className="list" key={index}>
            <input className="indexList" disabled={true} value={cantoIndex}/>
-           <TextareaAutosize maxRows={10} className="textinput" value={sectionTextArr[index]}/>
+           <TextareaAutosize spellcheck="false" maxRows={10} className="textinput" value={sectionTextArr[index]}/>
            </div>)
          )}
          </div>
